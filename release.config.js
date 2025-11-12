78a1446f4c53f68b8bf6ac8a6ddba6e95e07d1f6
GuiRBeira <guilhermericardobeira@gmail.com>
2025-11-12 15:40:55 -0300

    chore: Atualização da verificação de versões para chore, docs, etc, não quebrarem o pipeline de CI

--- release.config.js
+++ release.config.js
@@ -2,17 +2,40 @@
 module.exports = {
   branches: ["main"],
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+        releaseRules: [
+          { type: "chore", release: false },
+          { type: "docs", release: false },
+          { type: "style", release: false },
+          { type: "refactor", release: false },
+          { type: "test", release: false }
+        ],
+      },
+    ],
     "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
-    "@semantic-release/npm",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false, // evita publish no npm
+      },
+    ],
     "@semantic-release/github",
     [
       "@semantic-release/git",
       {
         assets: ["CHANGELOG.md", "package.json"],
-        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      }
-    ]
-  ]
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
 };
